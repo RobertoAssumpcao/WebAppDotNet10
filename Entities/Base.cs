fce42fd5a4d40e263bbd1b3423984f5637564faf
Roberto Assumpção <contatobetoassumpcao@icloud.com>
2026-01-19 01:24:22 -0300

    Adiciona classes Base e Notifica para entidades e notificações
    Foram criadas as classes Base (com propriedades Id e Nome) e Notifica (para controle de notificações) no namespace Entities, estabelecendo uma estrutura inicial para entidades com suporte a notificações e identificação. As propriedades de Notifica não são mapeadas para o banco nem exibidas no Swagger.

---
+++ Entities/Base.cs
@@ -0,0 +1,13 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace Entities
+{
+    public class Base : Notifica
+    {
+        [Display(Name = "Código")]
+        public Guid Id { get; set; }
+
+        [Display(Name = "Nome")]
+        public string Nome { get; set; }
+    }
+}
---
+++ Entities/Notifica.cs
@@ -0,0 +1,25 @@
+﻿using System.ComponentModel.DataAnnotations.Schema;
+using System.Text.Json.Serialization;
+
+namespace Entities
+{
+    public class Notifica
+    {
+        public Notifica()
+        {
+            Notificacoes = [];
+        }
+
+        [JsonIgnore] //Não aparece no swagger
+        [NotMapped]
+        public string? NomePropiedade { get; set; }
+
+        [JsonIgnore] //Não aparece no swagger
+        [NotMapped]
+        public string? Mensagem { get; set; }
+
+        [JsonIgnore] //Não aparece no swagger
+        [NotMapped]
+        public List<Notifica> Notificacoes;
+    }
+}
